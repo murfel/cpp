4a99cb1d20b197185bcceb557c529def917b887d
Natasha Murashkina <murnatty@gmail.com>
2017-02-24 11:22:11 +0300

    12: Basic implementation.

--- lab_12/include/my_vector.h
+++ lab_12/include/my_vector.h
@@ -1,4 +1,8 @@
 #include <cstddef>
+#include <iostream>
+#include <typeinfo>
+
+#define INIT_CAPACITY 4
 
 template <class T>
 class my_vector{
@@ -9,14 +13,15 @@ public:
     my_vector& operator=(my_vector& other);
     ~my_vector();
 
-    size_t size();
-    size_t capacity();
-    bool empty();
+    size_t size() const;
+    size_t capacity() const;
+    bool empty() const;
 
     void resize(size_t n);
     void reserve(size_t n);
 
-    ?? operator[](size_t index);
+    T& operator[](size_t index) const;
+    T& operator[](size_t index);
 
     void push_back(T& t);
     void pop_back();
@@ -26,4 +31,126 @@ private:
     size_t capacity_;
     size_t size_;
     T* array_;
+    void init(size_t n);
 };
+
+size_t round(size_t n) {
+    size_t rounded = 1;
+    while (rounded < n) {
+        rounded *= 2;
+    }
+    return rounded;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const my_vector<T>& o) {
+    for (size_t i = 0; i < o.size(); i++) {
+        os << o[i] << " ";
+    }
+    return os;
+}
+
+template <class T>
+void my_vector<T>::init(size_t n) {
+    size_ = n;
+    capacity_ = (n == 0) ? INIT_CAPACITY : round(capacity_);
+    array_ = new T [capacity_];
+}
+
+template <class T>
+my_vector<T>::my_vector() {
+    init(0);
+}
+
+template <class T>
+my_vector<T>::my_vector(size_t n) {
+    init(n);
+}
+
+template <class T>
+my_vector<T>::my_vector(my_vector& other) {
+    init(other.size());
+    for (int i = 0; i < size_; i++) {
+        array_[i] = other[i];
+    }
+}
+
+template <class T>
+my_vector<T>& my_vector<T>::operator=(my_vector& other) {
+    resize(other.size());
+    for (int i = 0; i < size_; i++) {
+        array_[i] = other[i];
+    }
+}
+
+template <class T>
+my_vector<T>::~my_vector() {
+    delete[] array_;
+}
+
+template <class T>
+size_t my_vector<T>::size() const {
+    return size_;
+}
+
+template <class T>
+size_t my_vector<T>::capacity() const {
+    return capacity_;
+}
+
+template <class T>
+bool my_vector<T>::empty() const {
+    return size_ == 0;
+}
+
+template <class T>
+void my_vector<T>::resize(size_t n) {
+    reserve(n);
+    size_ = n;
+}
+
+template <class T>
+void my_vector<T>::reserve(size_t n) {
+    if (capacity_ < n) {
+        T* temp = new T [round(n)];
+        for (int i = 0; i < size_; i++) {
+            temp[i] = array_[i];
+        }
+        delete[] array_;
+        array_ = temp;
+    }
+}
+
+template <class T>
+T& my_vector<T>::operator[](size_t index) const {
+    return array_[index];
+}
+
+template <class T>
+T& my_vector<T>::operator[](size_t index) {
+    return array_[index];
+}
+
+template <class T>
+void my_vector<T>::push_back(T& t) {
+    resize(size_ + 1);
+    array_[size_ - 1] = t;
+}
+
+template <class T>
+void my_vector<T>::pop_back() {
+    size_--;
+}
+
+template <class T>
+void my_vector<T>::clear() {
+    while (size_--) {
+        delete array_[size_ - 1];
+    }
+}
+
+template <class T>
+void test_my_vector<T>(T& o1, T& o2) {
+    v = my_vector<T>();
+    assert v.size() == 0;
+}
--- lab_12/src/main.cpp
+++ lab_12/src/main.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
+#include <string>
 
 #include "my_vector.h"
 
 class Product {
 public:
-    Product(const char* name, int quantity, double price);
-
+    Product(const char* name, int quantity, double price) : name_((char *)name), quantity_(quantity), price_(price) { }
+    std::string name() const {
+        return std::string(name_);
+    }
+    int quantity() const {
+        return quantity_;
+    }
+    double price() const {
+        return price_;
+    }
 private:
     char *name_;
     int quantity_;
     double price_;
 };
 
+std::ostream& operator<<(std::ostream& os, Product& o) {
+    os << o.name() << " ";
+    os << o.quantity() << " ";
+    os << o.price() << " ";
+    return os;
+}
+
 void print(const my_vector<Product>& v) {
     std::cout << v << std::endl;
 }
 
 int main() {
-    //test_my_vector<int>(5, 10);
-    //test_my_vector<Product>(Product("asdf", 4, 12.0), Product("qwe", -1, 7.5));
+    test_my_vector<int>(5, 10);
+    test_my_vector<Product>(Product("asdf", 4, 12.0), Product("qwe", -1, 7.5));
 
     return 0;
 }
