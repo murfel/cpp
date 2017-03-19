f45ebde0164fe1160d5e8723796488c0596c000f
Natasha Murashkina <murnatty@gmail.com>
2017-03-19 15:29:38 +0300

    14: Move try-catch to outerior

--- lab_14/src/main.cpp
+++ lab_14/src/main.cpp
@@ -21,61 +21,53 @@ int main(int, char **) {
 
   while (1) {
     cin >> cmd;
-    if (cmd == "exit") {
-      break;
-    } else if (cmd == "print") {
-      cin >> reg1;
-      for (size_t i = 0; i < regs[reg1]->get_rows(); ++i) {
-        for (size_t j = 0; j < regs[reg1]->get_cols(); ++j) {
-          cout << regs[reg1]->get(i, j) << " ";
-        }
-        cout << endl;
-      }
-    } else if (cmd == "load") {
 
-      cin >> reg1 >> fname;
-      delete regs[reg1];
-
-      file.open(fname, fstream::in);
-      file >> rows >> cols;
-      regs[reg1] = new Matrix(rows, cols);
-      for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            file >> val;
-            regs[reg1]->set(i, j, val);
+    try {
+      if (cmd == "exit") {
+        break;
+      } else if (cmd == "print") {
+        cin >> reg1;
+        for (size_t i = 0; i < regs[reg1]->get_rows(); ++i) {
+          for (size_t j = 0; j < regs[reg1]->get_cols(); ++j) {
+            cout << regs[reg1]->get(i, j) << " ";
+          }
+          cout << endl;
         }
-      }
-      file.close();
-    } else if (cmd == "add") {
-      cin >> reg1 >> reg2;
-      try {
+      } else if (cmd == "load") {
+
+        cin >> reg1 >> fname;
+        delete regs[reg1];
+
+        file.open(fname, fstream::in);
+        file >> rows >> cols;
+        regs[reg1] = new Matrix(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+          for (int j = 0; j < cols; ++j) {
+              file >> val;
+              regs[reg1]->set(i, j, val);
+          }
+        }
+        file.close();
+      } else if (cmd == "add") {
+        cin >> reg1 >> reg2;
         *regs[reg1] += *regs[reg2];
-      }
-      catch (MatrixException& e) {
-        cout << e.what() << endl;
-      }
-    } else if (cmd == "mul") {
-      cin >> reg1 >> reg2;
-      try {
+      } else if (cmd == "mul") {
+        cin >> reg1 >> reg2;
         *regs[reg1] *= *regs[reg2];
-      }
-      catch (MatrixException& e) {
-        cout << e.what() << endl;
-      }
-    } else if (cmd == "elem") {
-      cin >> reg1 >> row_index >> col_index;
-      try {
+      } else if (cmd == "elem") {
+        cin >> reg1 >> row_index >> col_index;
         cout << regs[reg1]->get(row_index, col_index);
       }
-      catch (MatrixException& e) {
-        cout << e.what() << endl;
-      }
+    }
+    catch (MatrixException& e) {
+      cout << e.what() << endl;
     }
   }
 
   for (map<string, Matrix*>::iterator it = regs.begin(); it != regs.end(); ++it) {
     delete it->second;
   }
+
 
   return 0;
 }
