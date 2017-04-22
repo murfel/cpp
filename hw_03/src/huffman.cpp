25b2430b07537e80370138d8cff995912b94bcb4
Natasha Murashkina <murnatty@gmail.com>
2017-04-22 13:46:32 +0300

    hw3: Rearchitecture. HuffTree is working.

--- hw_03/src/huffman.cpp
+++ hw_03/src/huffman.cpp
@@ -1,17 +1,6 @@
 #include "huffman.h"
 #include <bits/stdc++.h>
 
-void HuffTree::build_code(size_t index, std::string s) {
-    if (index == static_cast<std::size_t>(-1)) { index = root; s = ""; }
-    if (tree[index].left == static_cast<std::size_t>(-1)) {
-        symbol_to_code[tree[index].symbol] = s;
-    }
-    else {
-        build_code(tree[index].left, s + "0");
-        build_code(tree[index].right, s + "1");
-    }
-}
-
 BinaryOfstream& BinaryOfstream::operator<<(std::string s) {
     for (std::size_t i = 0; i < s.length(); ++i) {
         if (counter_ == 8) {
@@ -31,19 +20,12 @@ void BinaryOfstream::flush() {
     buffer_ = 0;
 }
 
-void compress(std::string input_file, std::string output_file) {
-    std::vector<int> frequencies(256, 0);
-    std::ifstream ifs(input_file.c_str());
-    char c;
-    while (ifs.get(c)) {
-        frequencies[static_cast<unsigned char>(c)]++;
-    }
-    ifs.close();
-    HuffTree tree;
-    std::priority_queue<std::pair<int, int>> new_frequencies;
+HuffTree::HuffTree(std::vector<int> & frequencies) {
+    tree_.resize(frequencies.size());
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> new_frequencies;
     for (size_t i = 0; i < frequencies.size(); ++i) {
         if (frequencies[i]) {
-            tree.insert_node(-1, -1, -1, i);
+            tree_[i] = {-1, -1, -1};
             new_frequencies.push({frequencies[i], i});
         }
     }
@@ -52,20 +34,45 @@ void compress(std::string input_file, std::string output_file) {
         new_frequencies.pop();
         std::pair<int, int> min2 = new_frequencies.top();
         new_frequencies.pop();
-        tree.insert_node(-1, min1.second, min2.second, frequencies.size());
+        tree_.push_back({-1, min1.second, min2.second});
         new_frequencies.push({min1.first + min2.first, frequencies.size()});
         frequencies.push_back(min1.first + min2.first);
     }
-    tree.set_root(new_frequencies.top().second);
-    tree.build_code();
+    root_ = new_frequencies.top().second;
+}
 
+void HuffTree::build_code(int index, std::string s) {
+    if (index == -1) { index = root_; s = ""; }
+    if (tree_[index].left == -1) {
+        symbol_to_code_[index] = s;
+    }
+    else {
+        build_code(tree_[index].left, s + "0");
+        build_code(tree_[index].right, s + "1");
+    }
+}
 
+void count_frequencies(std::string input_file, std::vector<int> & frequencies) {
+    std::ifstream ifs(input_file.c_str());
+    char c;
+    while (ifs.get(c)) {
+        frequencies[static_cast<unsigned char>(c)]++;
+    }
+}
 
-
-
-//    for (auto i : tree_.symbol_to_code) {
-//        std::cerr << i.first << " " << i.second << std::endl;
+void compress(std::string input_file, std::string output_file) {
+    std::vector<int> frequencies(256, 0);
+    count_frequencies(input_file, frequencies);
+//    for (auto f : frequencies) {
+//        if (f) std::cerr << f << "\n";
 //    }
+    HuffTree tree(frequencies);
+    tree.build_code();
+//    std::cerr << tree.get_code_of('a') << "\n";
+//    std::cerr << tree.get_code_of('b') << "\n";
+//    std::cerr << tree.get_code_of('c') << "\n";
+//    std::cerr << tree.get_code_of('d') << "\n";
+
 //    std::ofstream ofs(output_file.c_str());
 //    BinaryOfstream bofs(ofs);
 //
--- hw_03/src/huffman.h
+++ hw_03/src/huffman.h
@@ -4,33 +4,18 @@
 
 class HuffTree {
 public:
-    size_t get_root() const { return root; }
-    void set_root(size_t new_root) { root = new_root; }
-    void insert_node(std::size_t parent, std::size_t left, std::size_t right, std::size_t symbol) {
-        std::cerr << std::max(std::max(parent, left), std::max(right, symbol)) << std::endl;
-        tree.reserve(std::max(std::max(parent, left), std::max(right, symbol)));
-        tree[symbol] = {parent, left, right, symbol};
-        tree[left].parent = symbol;
-        tree[right].parent = symbol;
-        if (parent == static_cast<std::size_t >(-1)) {
-            root = symbol;
-        }
-    }
-    size_t get_left_of(std::size_t symbol) { return tree[symbol].left; }
-    size_t get_right_of(std::size_t symbol) { return tree[symbol].right; }
-    std::string get_code_of(std::size_t symbol) { return symbol_to_code[symbol]; }
-    void build_code(size_t index = -1, std::string s = "");
+    HuffTree(std::vector<int> & frequencies);
+    void build_code(int index = -1, std::string s = "");
+    std::string get_code_of(int symbol) { return symbol_to_code_[symbol]; }
 private:
     struct TreeNode {
-        std::size_t parent;
-        std::size_t left;
-        std::size_t right;
-        std::size_t symbol;
+        int parent;
+        int left;
+        int right;
     };
-    std::vector<TreeNode> tree;
-    std::size_t root;
-    std::unordered_map<std::size_t, std::string> symbol_to_code;
-
+    std::vector<TreeNode> tree_;
+    std::size_t root_;
+    std::unordered_map<std::size_t, std::string> symbol_to_code_;
 };
 
 class BinaryOfstream {
@@ -44,5 +29,6 @@ private:
     int counter_;
 };
 
+void count_frequencies(std::string input_file, std::vector<int> & frequencies);
 void compress(std::string input_file, std::string output_file);
 void decompress(std::string input_file, std::string output_file);
