a1c4dcd90fa5ecaad8e776bfe86985ada21bec8b
Natasha Murashkina <murnatty@gmail.com>
2017-05-28 21:51:10 +0300

    15: Pass 1 smoke.

--- lab_15/include/linq.h
+++ lab_15/include/linq.h
@@ -40,9 +40,9 @@ public:
 //  }
 
 
-//  auto drop(int count) {
-//    return drop_enumerator<T>(*this, count);
-//  }
+  auto drop(int count) {
+    return drop_enumerator<T>(*this, count);
+  }
 
   template<typename U = T, typename F>
   auto select(F func) {
@@ -53,11 +53,15 @@ public:
 //  auto until(F func) {
 //    return until_enumerator<T, F>(*this, func);
 //  }
-//
-//  template<typename F>
-//  auto where(F predicate) {
-//    return where_enumerator<T, F>(*this, predicate);
-//  }
+
+  template<typename F>
+  auto where(F predicate) {
+    return where_enumerator<T, F>(*this, predicate);
+  }
+
+  auto where_neq(T value) {
+    return this->where([value](const T& x) { return x != value; });
+  }
 
   std::vector<T> to_vector() {
     std::vector<T> result;
@@ -103,40 +107,40 @@ auto from(Iter begin, Iter end) {
   return range_enumerator<typename std::iterator_traits<Iter>::value_type, Iter>(begin, end);
 }
 
-//template<typename T>
-//class drop_enumerator : public enumerator<T> {
-//public:
-//  drop_enumerator(enumerator<T> &parent, int count) : parent_(parent), count_(count) {}
-//  virtual const T& operator*() const override {
-//    drop();
-//    if (count_)
-//      throw std::runtime_error("Attemp to dereference an empty Drop enumerator.");
-//    return *parent_;
-//  }
-//  virtual enumerator<T>& operator++() override {
-//    drop();
-//    if (count_)
-//      throw std::runtime_error("Attemp to increment past-the-end Drop enumerator.");
-//    ++parent_;
-//    return *this;
-//  }
-//  virtual operator bool() const override {
-//    drop();
-//    return (count_ ? false : parent_);
-//  }
-//private:
-//  void drop() {
-//    while (parent_)
-//      if (count_) {
-//        ++parent_;
-//        --count_;
-//      }
-//      else
-//        break;
-//  }
-//  enumerator<T> &parent_;
-//  int count_;
-//};
+template<typename T>
+class drop_enumerator : public enumerator<T> {
+public:
+  drop_enumerator(enumerator<T> &parent, int count) : parent_(parent), count_(count) {}
+  virtual const T& operator*() const override {
+    drop();
+    if (count_)
+      throw std::runtime_error("Attemp to dereference an empty Drop enumerator.");
+    return *parent_;
+  }
+  virtual enumerator<T>& operator++() override {
+    drop();
+    if (count_)
+      throw std::runtime_error("Attemp to increment past-the-end Drop enumerator.");
+    ++parent_;
+    return *this;
+  }
+  virtual operator bool() const override {
+    drop();
+    return (count_ ? false : parent_);
+  }
+private:
+  void drop() const {
+    while (parent_)
+      if (count_) {
+        ++parent_;
+        --count_;
+      }
+      else
+        break;
+  }
+  enumerator<T> &parent_;
+  mutable int count_;
+};
 
 
 template<typename T, typename U, typename F>
@@ -144,7 +148,8 @@ class select_enumerator : public enumerator<T> {
 public:
   select_enumerator(enumerator<U> &parent, F func) : parent_(parent), func_(func) {}
   virtual const T& operator*() const override {
-    return func_(*parent_);
+    last_elem_ = func_(*parent_);
+    return last_elem_;
   }
   virtual select_enumerator& operator++() override {
     ++parent_;
@@ -157,6 +162,7 @@ public:
 private:
   enumerator<U> & parent_;
   F func_;
+  mutable T last_elem_;
 };
 
 //template<typename T, typename F>
@@ -170,50 +176,29 @@ private:
 //  F predicate_;
 //};
 
-//template<typename T, typename F>
-//class where_enumerator : public enumerator<T> {
-//public:
-//  where_enumerator(enumerator<T> &parent, F predicate) : parent_(parent), predicate_(predicate) {}
-//  // invariant: мы стоим на неизвестном месте (но нельзя: мы стоим на ок элементе -- таких может не быть вообще)
-//  virtual const T& operator*() {
-//    while (parent_)
-//      if (func_(*parent_))
-//        return *parent_;
-//      else
-//        ++parent_;
-//    throw std::runtime_error("Attemp to dereference an empty enumerator.");
-//  }
-//  virtual enumerator<T>& operator++() {
-//    // Найти ближайший валидный (мб текущий), и шагнуть за него
-//    while (parent_) {
-//      while (!func_(*parent_))
-//        ++parent_;
-//      ++parent_;
-//    }
-//    return *this;
-//  }
-//  virtual operator bool() {
-//    while (parent_)
-//      if (func_(*parent_))
-//        return true;
-//      else
-//        ++parent_;
-//    return false;
-//  }
-//private:
-//  enumerator<T> &parent_;
-//  F predicate_;
-//};
-//
-//template<typename T>
-//class where_neq_enumerator : public enumerator<T> {
-//public:
-//  where_neq_enumerator(enumerator<T> &parent, T value) {
-////    auto where_enum_ = where_enumerator<T, int*>(parent, [&value](int x) { return x != value; });
-//  }
-//private:
-////  where_enumerator<T, >
-//};
-//
+template<typename T, typename F>
+class where_enumerator : public enumerator<T> {
+public:
+  where_enumerator(enumerator<T> &parent, F predicate) : parent_(parent), predicate_(predicate) {}
+  // invariant: мы стоим на неизвестном месте (но нельзя: мы стоим на ок элементе -- таких может не быть вообще)
+  virtual const T& operator*() const override {
+    operator bool();
+    return *parent_;
+  }
+  virtual enumerator<T>& operator++() override {
+    // Найти ближайший валидный (мб текущий), и шагнуть за него
+    operator bool();
+    ++parent_;
+    return *this;
+  }
+  virtual operator bool() const override {
+    while (parent_ && !predicate_(*parent_))
+      ++parent_;
+    return parent_ && predicate_(*parent_);
+  }
+private:
+  enumerator<T> &parent_;
+  F predicate_;
+};
 
 #endif
--- lab_15/minimal_smoke_test.cpp
+++ lab_15/minimal_smoke_test.cpp
@@ -19,16 +19,16 @@ void from_select() {
   assert(res == expected);
 }
 
-//void from_drop_select() {
-//  const int xs[] = { 1, 2, 3 };
-//  std::vector<int> res = from(xs, xs + 3).drop(1).select([](int x) { return x + 5; }).to_vector();
-//  std::vector<int> expected = { 7, 8 };
-//  assert(res == expected);
-//}
+void from_drop_select() {
+  const int xs[] = { 1, 2, 3 };
+  std::vector<int> res = from(xs, xs + 3).drop(1).select([](int x) { return x + 5; }).to_vector();
+  std::vector<int> expected = { 7, 8 };
+  assert(res == expected);
+}
 
 int main() {
   from_to_vector();
   from_select();
-//  from_drop_select();
+  from_drop_select();
   return 0;
 }
